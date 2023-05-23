e45dc1953e5c33c732bf8fa09b81e3991fe18f3b
jmartinez@leadclic.com <jmartinez@leadclic.com>
2023-05-23 08:47:41 +0200

    v0.0.1

--- index.js
+++ index.js
@@ -12,12 +12,31 @@ const publicPath = path.join(__dirname, '/');
 app.use(express.static(publicPath));
 
 const port = 3000;
-http.listen(port, () => {
+server.listen(port, () => {
     console.log(`Servidor en funcionamiento en el puerto ${port}`);
 });
 
+const players = []; // Array para almacenar la información de los jugadores conectados
+
 io.on('connection', (socket) => {
     console.log('Un cliente se ha conectado');
+
+    // Asignar un número de jugador y recursos iniciales al cliente conectado
+    const playerId = players.length + 1; // Número de jugador basado en la cantidad de jugadores conectados
+    const initialResources = {
+        gold: 5,
+        coffeeBeans: 2,
+        groundCoffee: 0
+    };
+
+    // Agregar la información del jugador conectado al array de jugadores
+    players.push({
+        playerId,
+        resources: initialResources
+    });
+
+    // Enviar la información de todos los jugadores a cada cliente
+    socket.emit('player_data', players);
 
     socket.on('turn_ended', (message) => {
         console.log('El jugador ha finalizado su turno:', message);
@@ -28,6 +47,11 @@ io.on('connection', (socket) => {
 
     socket.on('disconnect', () => {
         console.log('Un cliente se ha desconectado');
+        // Eliminar la información del jugador desconectado del array de jugadores
+        const playerIndex = players.findIndex((player) => player.playerId === playerId);
+        if (playerIndex !== -1) {
+            players.splice(playerIndex, 1);
+        }
         // Lógica relacionada con la desconexión del jugador
         // ...
     });
--- js/main.js
+++ js/main.js
@@ -1,8 +1,6 @@
-import * as PIXI from 'pixi.js';
-
 const app = new PIXI.Application({
-    width: 800,
-    height: 600,
+    width: 1600,
+    height: 900,
     backgroundColor: 0x1099bb
 });
 
@@ -11,12 +9,59 @@ document.body.appendChild(app.view);
 // Conexión con Socket.IO
 const socket = io();
 
-// Escucha el evento click del botón "Fin de turno"
-const endTurnButton = document.getElementById('endTurnButton');
-endTurnButton.addEventListener('click', () => {
-    const message = 'El jugador ha finalizado su turno';
-    // Emitir el evento al servidor
-    socket.emit('turn_ended', message);
-});
+// Array para almacenar las referencias de los textos de los jugadores
+const playerTexts = [];
 
-// Tu código de juego aquí
+// Crear un botón con recuadro en PixiJS
+const button = new PIXI.Container();
+
+// Resto del código del botón...
+
+// Escucha el evento 'player_data' enviado por el servidor
+socket.on('player_data', (players) => {
+    // Limpiar los textos existentes antes de actualizarlos
+    playerTexts.forEach((text) => {
+        app.stage.removeChild(text);
+    });
+    playerTexts.length = 0;
+
+    // Recorrer los datos de los jugadores y crear los textos correspondientes
+    players.forEach((player, index) => {
+        const { playerId, resources } = player;
+
+        const playerText = new PIXI.Text(`Jugador ${index + 1}:`, {
+            fill: 'white',
+            fontSize: 18,
+        });
+        playerText.position.set(10, 30 + index * 60);
+        app.stage.addChild(playerText);
+        playerTexts.push(playerText);
+
+        const goldText = new PIXI.Text(`Oro: ${resources.gold}`, {
+            fill: 'white',
+            fontSize: 18,
+        });
+        goldText.position.set(10, 60 + index * 60);
+        app.stage.addChild(goldText);
+        playerTexts.push(goldText);
+
+        const coffeeBeansText = new PIXI.Text(`Granos de café: ${resources.coffeeBeans}`, {
+            fill: 'white',
+            fontSize: 18,
+        });
+        coffeeBeansText.position.set(10, 90 + index * 60);
+        app.stage.addChild(coffeeBeansText);
+        playerTexts.push(coffeeBeansText);
+
+        const groundCoffeeText = new PIXI.Text(`Café molido: ${resources.groundCoffee}`, {
+            fill: 'white',
+            fontSize: 18,
+        });
+        groundCoffeeText.position.set(10, 120 + index * 60);
+        app.stage.addChild(groundCoffeeText);
+        playerTexts.push(groundCoffeeText);
+    });
+
+    // Resto de la lógica del juego
+    // ...
+});
